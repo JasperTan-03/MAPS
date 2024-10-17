875ab9739fbce439de02e90e1fb4372835d0de40
HaakonMongstad <haakon@mongstad.net>
2024-10-16 21:05:34 -0500

    load point cloud in chunks

--- build_network.py
+++ build_network.py
@@ -1,8 +1,20 @@
 import argparse
 
 import numpy as np
+import pandas as pd
 import networkx as nx
 from sklearn.neighbors import KDTree
+from tqdm import tqdm
+
+def load_point_cloud_chunks(filename, chunk_size=1000000):
+    chunks = []
+    for chunk in pd.read_csv(filename, sep=" ", header=None, chunksize=chunk_size):
+        chunks.append(chunk)
+
+    data = pd.concat(chunks)
+    points = data.iloc[:, :3].values 
+    colors = data.iloc[:, 3:].values
+    return points, colors
 
 
 def load_point_cloud(filename):
@@ -15,18 +27,21 @@ def load_point_cloud(filename):
 
 def build_knn_network(points: np.ndarray, colors: np.ndarray, k: int = 5):
 
+    print("Building KDTree")
     tree = KDTree(points)
     distances, indices = tree.query(points, k=k + 1)
 
     # Create a graph
     G = nx.Graph()
 
-    for i in range(points.shape[0]):
+    print("Creating Nodes")
+    for i in tqdm(range(points.shape[0])):
         intensity, r, g, b = colors[i]
         x, y, z = points[i]
         G.add_node(i, x=x, y=y, z=z, intensity=intensity, r=r, g=g, b=b)
 
-    for i in range(points.shape[0]):
+    print("Creating Edges")
+    for i in tqdm(range(points.shape[0])):
         for j in range(1, k + 1):
             G.add_edge(i, indices[i, j], weight=distances[i, j])
 
@@ -47,7 +62,7 @@ if __name__ == "__main__":
 
     data = args.point_cloud
 
-    points, colors = load_point_cloud(data)
+    points, colors = load_point_cloud_chunks(data, chunk_size=10000)
 
     graph = build_knn_network(points, colors, k=5)
 
