0c2547e6c2812a0fe1c57544fe2ca082e361553d
JasperTan-03 <jaspertan@utexas.edu>
2024-11-17 05:09:49 -0600

    Clean up dqn

--- 2d_maps/dqn.py
+++ 2d_maps/dqn.py
@@ -1,33 +1,12 @@
-import math
 import random
 from collections import deque, namedtuple
-from itertools import count
-from typing import Dict, List, Optional, Tuple
+from typing import Tuple
 
-import gymnasium as gym
-import matplotlib
-import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
 
-from rl_environment import SegmentationEnv
-
-# set up matplotlib
-is_ipython = "inline" in matplotlib.get_backend()
-if is_ipython:
-    from IPython import display
-
-plt.ion()
-
-# if GPU is to be used
-device = torch.device(
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available() else "cpu"
-)
-
+# Define the transition tuple
 Transition = namedtuple(
     "Transition", ("state", "action", "classification", "next_state", "reward")
 )
@@ -66,13 +45,7 @@ class DQN(nn.Module):
         self.layer2 = nn.Linear(hidden_dim, hidden_dim)
         self.layer3 = nn.Linear(hidden_dim, num_actions)
 
-    def forward(
-        self, state_input: torch.Tensor, gcn_features: torch.Tensor
-    ) -> torch.Tensor:
-
-        # Combine inputs
-        x = torch.cat([state_input, gcn_features], dim=1)
-
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Forward pass
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
